d50cfb047904497a93b01c878fbe882d2d81e823
SHUMBUSHO123 <teacherbubazi@gmail.com>
2026-07-30 23:40:54 +0200

    login and register redesigned

--- lesson-plan-generator/backend/cbc_backend/urls.py
+++ lesson-plan-generator/backend/cbc_backend/urls.py
@@ -4,6 +4,9 @@ from django.http import FileResponse
 from django.conf import settings
 from django.conf.urls.static import static
 import os
+from lessons import views
+from lessons.views_separated.payment_views import payment_page
+
 
 def service_worker(request):
     return FileResponse(
@@ -11,8 +14,13 @@ def service_worker(request):
         content_type='application/javascript'
     )
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.landing, name='landing'),
+    path('index/', views.index, name='index'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('payment/', payment_page, name='payment'),
     path('sw.js', service_worker),
     path('api/', include('lessons.urls')),
 ]
